654c1cc97c7f6fb1943bd70a2fce44a02a46d01c
Joe Christensen <joechristensenwork@gmail.com>
2021-08-30 15:41:44 -0700

    commit tree 8cf89930f4cfba93ea8973299ec3ae649f6e7720

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,13 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: ["Fiction", "Non-Fiction"] }
+    validate :title_must_include_keywords
+
+    def title_must_include_keywords
+        unless [/Won't Believe/, /Secret/, /Top \d/, /Guess/].any? { |keyword| keyword.match title}
+            errors.add(:title, "Title does not include valid keywords")
+        end
+    end
 end
